d4a806d89bf16ab9a2db9e389a0edd332627bd75
Rohan v. Kulkarni <rohan.kulkarni@gatewaygroup.com>
2020-12-05 16:39:31 +0200

    Initial commit

---
+++ src/webparts/myLinks/MyLinksWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'MyLinksWebPartStrings';
+import MyLinks from './components/MyLinks';
+import { IMyLinksProps } from './components/IMyLinksProps';
+
+export interface IMyLinksWebPartProps {
+  description: string;
+}
+
+export default class MyLinksWebPart extends BaseClientSideWebPart<IMyLinksWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IMyLinksProps> = React.createElement(
+      MyLinks,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/myLinks/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyLinksWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyLinksWebPartStrings' {
+  const strings: IMyLinksWebPartStrings;
+  export = strings;
+}
